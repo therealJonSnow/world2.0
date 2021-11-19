fcb57152f2da4e402165e628cb3ca0c373e02a16
Jon Snow <47143928+therealJonSnow@users.noreply.github.com>
2021-11-19 09:17:55 +0000

    init commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cad9bee2bc65df628fedee7bb4a81d35'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e457d025-29c5-4fa9-afe3-964bb67133d1'
+};
