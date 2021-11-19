4041794958f6c0d0a61938778346316fd2456737
Jon Snow <47143928+therealJonSnow@users.noreply.github.com>
2021-11-19 09:29:36 +0000

    frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+let counter = ref(0)
+
+setInterval(() => {
+  counter.value++
+}, 1000)
+</script>
+
+<template>
+  <div>
+    <header class="bg-white shadow flex px-8 py-8" v-if="$route.meta.title">
+      <img class="w-32 sm:w-64" src="./assets/logo.png" alt="">
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
